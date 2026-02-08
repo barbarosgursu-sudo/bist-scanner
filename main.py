6863786c0b36a978260fe4d50534a46da770ef90
barbarosgursu-sudo <barbaros.gursu@gmail.com>
2026-02-08 18:27:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from fastapi import FastAPI
 from apscheduler.schedulers.background import BackgroundScheduler
 from contextlib import asynccontextmanager
 
-GAS_BASE_URL = "https://script.google.com/macros/s/AKfycbyF59xn_O6OBxV_rPVm4EMWI2bKuL7DiMEi4WYYh1UfVjgY2SGbUTVmGDpsikzEhho9/exec"
+GAS_BASE_URL = "https://script.google.com/macros/s/AKfycbwOAbNH6O5wEUYoqqOduGA7vYewXMJ9n4EFeGFMbZZJPOQOSDvCeMeJWhpKcqcmyqiu/exec"
 TR_TZ = pytz.timezone("Europe/Istanbul")
 BATCH_SIZE = 50
 
